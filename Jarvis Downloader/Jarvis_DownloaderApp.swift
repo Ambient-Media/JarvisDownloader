3e305d09fd4ba6cf33507c876e3dba7d262cea67
Jarvis <noahreich406@gmail.com>
2026-01-04 11:52:33 -0700

    Initial Commit

---
+++ Jarvis Downloader/Jarvis_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jarvis_DownloaderApp.swift
+//  Jarvis Downloader
+//
+//  Created by Noah  on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Jarvis_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
